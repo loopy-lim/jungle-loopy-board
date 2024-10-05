48b1b68e5739962989139dd7895a3ddef4fd43d5
loopy-lim <loopy@jnu.ac.kr>
2024-10-05 20:42:06 +0900

    feat: add class-validator

--- jungle-loopy-board-be/src/auth/dto/auth.dto.ts
+++ jungle-loopy-board-be/src/auth/dto/auth.dto.ts
@@ -1,20 +1,40 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class AuthResponseDto {
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
 
 export class SignUpResponseDto {
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
+
+  @IsNotEmpty()
   readonly name: string;
 }
 
 export class DeleteAccountResponseDto {
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
 
 export class RefreshResponseDto {
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly name: string;
 }
--- jungle-loopy-board-be/src/users/dto/user.dto.ts
+++ jungle-loopy-board-be/src/users/dto/user.dto.ts
@@ -1,11 +1,25 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
 
 export class UpdateUserDto {
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
